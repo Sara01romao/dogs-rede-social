1288d761b8433438575cd745aa5d3ffbc82dcb3b
Sara Romão <sara-romao@live.com>
2022-03-17 23:08:50 -0300

    useContext

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import Footer from './Components/Footer';
 import Header from './Components/Header';
 import Home from './Components/Home';
 import Login from './Components/Login/Login';
+import UserStorage from './UserContext';
+
 
 
 
@@ -11,12 +13,14 @@ function App() {
   return (
     <div className="App">
       <BrowserRouter>
-        <Header/>
-        <Routes>
-          <Route path='/' element={<Home/>}/>
-          <Route path='/login/*' element={<Login/>}/>
-        </Routes>
-        <Footer/>
+      <UserStorage>
+          <Header/>
+          <Routes>
+            <Route path='/' element={<Home/>}/>
+            <Route path='/login/*' element={<Login/>}/>
+          </Routes>
+          <Footer/>
+        </UserStorage>
       </BrowserRouter>
        
     </div>
