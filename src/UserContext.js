2b299f6f9c5c2dc5a124143636ada9bb62377248
Sara Romão <sara-romao@live.com>
2022-03-22 23:35:23 -0300

    Protected Route

--- src/UserContext.js
+++ src/UserContext.js
@@ -44,6 +44,8 @@ export default function UserStorage({children}) {
                 setLoading(false);
               }
                
+            }else{
+                setLogin(false)
             }
         }
         autoLogin();
