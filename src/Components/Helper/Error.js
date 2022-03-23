4095b737ca3cb5d97cec7d9f6c9f7965f427b65c
Sara Romão <sara-romao@live.com>
2022-03-22 23:21:43 -0300

    altera cor de erro

--- src/Components/Helper/Error.js
+++ src/Components/Helper/Error.js
@@ -3,5 +3,5 @@ import React from 'react'
 export default function Error({error}) {
     if(!error) return null;
 
-  return <p style={{color:'#fb1', margin:'1rem 0'}}>{error}</p>
+  return <p style={{color:'#f31', margin:'1rem 0'}}>{error}</p>
 }
