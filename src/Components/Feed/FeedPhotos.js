65ce08802a0e2543bb95a4f79b30b5ced71040f9
Sara Romão <sara-romao@live.com>
2022-03-25 16:58:50 -0300

    modal estrutura

--- src/Components/Feed/FeedPhotos.js
+++ src/Components/Feed/FeedPhotos.js
@@ -6,14 +6,14 @@ import Error from '../Helper/Error'
 import Loading from '../Helper/Loading';
 import styles from './FeedPhotos.module.css';
 
-export default function FeedPhotos() {
+export default function FeedPhotos({setModalPhoto}) {
   const {data, loading, error, request} = useFetch();
 
   useEffect(() =>{
       async function fetchPhotos(){
           const {url, options} = PHOTOS_GET({page: 1, total: 6, user: 0});
           const {response, json} = await request(url, options);
-          console.log(json)
+    
       }
       fetchPhotos();
   }, [request])
@@ -23,7 +23,7 @@ export default function FeedPhotos() {
   if(data)
   return (
     <ul className={`${styles.feed} animeLeft`}>
-        {data.map((photo) => <FeedPhotosItem key={photo.id} photo={photo}/>)}
+        {data.map((photo) => <FeedPhotosItem key={photo.id} photo={photo} setModalPhoto={setModalPhoto}/>)}
     </ul>
   )
   else return null;
