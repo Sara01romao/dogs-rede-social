e28ce96fe0e71cee2879df0f2bdd6f56c9526eae
Sara Romão <sara-romao@live.com>
2022-03-28 17:49:22 -0300

    grafico

--- src/Components/User/UserStats.js
+++ src/Components/User/UserStats.js
@@ -1,12 +1,33 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { STATS_GET } from '../../api';
+import useFetch from '../../Hooks/useFetch';
+import Error from '../Helper/Error';
 import Head from '../Helper/Head';
+import Loading from '../Helper/Loading';
+
+
+const UserStatsGraphs = React.lazy(() => import('./UserStatsGraphs'))
 
 export default function UserStats() {
+  const {data, error, loading, request} = useFetch();
+  useEffect(()=>{
+    async function getData(){
+      const {url, options} = STATS_GET();
+      await request(url, options)
+    }
+    getData();
+    
+  },[request])
 
+  if(loading) return <Loading/>
+  if(error) return <Error error={error}/>
+  if(data)
   return (
-    <div>
-         <Head title="Estatísticas" />
-      UserStats
-      </div>
+    <React.Suspense fallback={<div><Head title="Estatísticas" /> Não tem</div>}>
+       <Head title="Estatísticas" />
+       <UserStatsGraphs data={data}/>
+     
+    </React.Suspense>
   )
+  else return null;
 }
